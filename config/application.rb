393585c00589675d127fcb7ad57c4bd3b6df8daa
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-08-30 21:43:45 +0900

    日本語化・タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,13 @@ module GyakutenCloneGroup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.i18n.default_locale = :ja
     # Don't generate system test files.
     config.generators.system_tests = nil
   end

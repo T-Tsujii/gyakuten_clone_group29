631ab98c75c4a998601c216342ed48bdf3774492
Katsuya Minami <arms.mc.rp@gmail.com>
2020-09-02 15:48:19 +0900

    コントローラの処理を before_action で共通化

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,39 @@
 class MoviesController < ApplicationController
-  def index
+  before_action :set_movie, only: %i[show edit update destroy]
 
+  def index
+    @movies = Movie.order(id: :asc)
   end
-end
+
+  def show
+  end
+  
+  def create
+    movie = Movie.create!(movie_params)
+    redirect_to movie
+  end
+
+  def edit
+  end
+
+  def update
+    @movie.update!(movie_params)
+    redirect_to @movie
+  end
+
+  def destroy
+    @movie.destroy!
+    redirect_to @movie
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+  
+  def movie_params
+    params.require(:movie).permit(:title, :url)
+  end
+
+end  

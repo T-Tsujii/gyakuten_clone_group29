a828581119ed6feba2cc21c2658a9c4d3ec774c6
Ikko-T <skyblueboy32@gmail.com>
2020-10-23 16:38:19 +0900

    creating question_show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,19 @@
 class QuestionsController < ApplicationController
   def create
+    question = Question.create!(question_params)
+    redirect_to question
   end
 
   def index
+    @questions = Question.all.order(id: :desc)
   end
 
   def show
+    @question = Question.find(params[:id])
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :detail)
   end
 end

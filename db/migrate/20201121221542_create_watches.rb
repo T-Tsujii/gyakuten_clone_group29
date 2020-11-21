8228316a5934d6feb6f1d9aef77643bcce8defba
Ikko-T <skyblueboy32@gmail.com>
2020-11-22 07:33:05 +0900

    Generated watch join table

---
+++ db/migrate/20201121221542_create_watches.rb
@@ -0,0 +1,10 @@
+class CreateWatches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :watches do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

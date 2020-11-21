055d4a1cf2ece3b73c53906ced26f76e9d5d75f9
Ikko-T <skyblueboy32@gmail.com>
2020-11-21 16:30:01 +0900

    flash-messgae setup

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     answer = @question.answers.create!(answer_params)
-    redirect_to @question
+    redirect_to @question, notice: "投稿しました"
   end
 
   private

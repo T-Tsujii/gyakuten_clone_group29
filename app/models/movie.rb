1419e7fafb5075e75e1c163ab7168d3043677205
Ikko-T <skyblueboy32@gmail.com>
2020-11-22 07:38:30 +0900

    Configured a has_many :through association in the user, movie, and watch model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :watches
+  has_many :users, through: :watches
   validates :title , presence: true
   validates :url , presence: true
   validates :genre, presence: true

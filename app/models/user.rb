84cc4ebe508ce9fcac519b3e745441e706f68097
Ikko-T <skyblueboy32@gmail.com>
2020-11-23 07:26:53 +0900

    Set up validations and associations in user, movie, and watch model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :movies, dependent: :destroy
+  has_many :watches, dependent: :destroy
+  has_many :watched_movies, through: :watches, source: :movie
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

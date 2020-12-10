e230bf3bbb5286ef6ed06f8e93573cacccb19bb0
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-12-10 14:51:28 +0900

    app/admin/text_books.rbを編集

--- app/admin/text_books.rb
+++ app/admin/text_books.rb
@@ -6,6 +6,27 @@ ActiveAdmin.register TextBook do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :genre, :title, :content, :image
+  form do |f|
+      f.inputs "TextBooks" do
+        f.input :genre
+        f.input :title
+        f.input :content
+        f.input :image, :as => :file
+      end
+      f.actions
+    end
+    
+    show do |item_image|
+      attributes_table do
+        row :genre
+        row :title
+        row :content
+        # show画面で画像を表示するためのタグを追加
+        row :image do
+          image_tag(text_book.image.url)
+        end
+      end
+    end    
   #
   # or
   #

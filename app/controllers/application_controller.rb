ed7c92f2a728db96c4189a020944474b1729ec41
Chibariyooh <62876107+Chibariyooh@users.noreply.github.com>
2020-12-11 10:56:48 +0900

    phpテキストページの絞り込み

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-    # def set_search
-    #     @search = User.ransack(params[:q])
-    #     @users = @search.result
-    # end
 end
--- app/controllers/text_books_controller.rb
+++ app/controllers/text_books_controller.rb
@@ -2,10 +2,10 @@ class TextBooksController < ApplicationController
 
   def index
     if params[:genre].nil?
-      @q = TextBook.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails"]).ransack(params[:q])
+      @q = TextBook.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails","Php"]).ransack(params[:q])
       @text_books = @q.result
     else
-      @q = TextBook.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails","Php"]).ransack(params[:q])
+      @q = TextBook.where(genre: params[:genre]).ransack(params[:q])
       @text_books = TextBook.where(genre: params[:genre])
     end
   end

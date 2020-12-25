b960ba91b373a61d82782463faef99e35c8defbf
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-12-25 11:56:18 +0900

    readsコントローラ修正

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,10 +1,19 @@
 class ReadsController < ApplicationController
   def create
-    current_user.reads.create!(text_book_id: params[:text_book_id])
-    redirect_back(fallback_location: root_path)
+    @text_book = TextBook.find(params[:text_book_id])
+    current_user.reads.find_by(text_book_id: @text_book.id)
   end
   def destroy
-    current_user.reads.find_by(text_book_id: params[:text_book_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @text_book = TextBook.find(params[:text_book_id])
+    current_user.reads.find_by(text_book_id: @text_book.id).destroy!
   end
+  # def create
+  #   current_user.reads.create!(text_book_id: params[:text_book_id])
+  #   redirect_back(fallback_location: root_path)
+  # end
+  # def destroy
+  #   current_user.reads.find_by(text_book_id: params[:text_book_id]).destroy!
+  #   redirect_back(fallback_location: root_path)
+  # end
+  
 end

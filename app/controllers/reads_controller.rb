e38cfee52f8696fdc809ec843b50d89affc3c9a4
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-12-26 19:02:30 +0900

    readsコントローラ修正

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -2,7 +2,7 @@ class ReadsController < ApplicationController
 
   def create
     @text_book = TextBook.find(params[:text_book_id])
-    current_user.reads.create!(text_book_id: @text_book.id).save
+    current_user.reads.create!(text_book_id: @text_book.id)
   end
 
   def destroy

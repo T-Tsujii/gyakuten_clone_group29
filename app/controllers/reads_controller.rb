ca19b1d4cbca465e39da29cf4782411f6a320fd2
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-12-26 16:13:39 +0900

    コントローラの修正

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,19 +1,13 @@
 class ReadsController < ApplicationController
+
   def create
     @text_book = TextBook.find(params[:text_book_id])
-    current_user.reads.find_by(text_book_id: @text_book.id)
+    current_user.reads.create!(text_book_id: @text_book.id).save
   end
+
   def destroy
     @text_book = TextBook.find(params[:text_book_id])
     current_user.reads.find_by(text_book_id: @text_book.id).destroy!
   end
-  # def create
-  #   current_user.reads.create!(text_book_id: params[:text_book_id])
-  #   redirect_back(fallback_location: root_path)
-  # end
-  # def destroy
-  #   current_user.reads.find_by(text_book_id: params[:text_book_id]).destroy!
-  #   redirect_back(fallback_location: root_path)
-  # end
   
 end
--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,4 +1,5 @@
 class WatchesController < ApplicationController
+
   def create
     @movie = Movie.find(params[:movie_id])
     current_user.watches.create!(movie_id: @movie.id)
@@ -8,14 +9,5 @@ class WatchesController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     current_user.watches.find_by(movie_id: @movie.id).destroy!
   end
-
-  # def create
-  #   current_user.watches.create!(movie_id: params[:movie_id])
-  #   redirect_back(fallback_location: root_path)
-  # end
-
-  # def destroy
-  #   current_user.watches.find_by(movie_id: params[:movie_id]).destroy!
-  #   redirect_back(fallback_location: root_path)
-  # end
+  
 end
